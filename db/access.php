8be614a5ea0472ac06a68b928c555ff320b5f9d2
nise <niels.seidel@nise81.com>
2025-04-17 10:11:47 +0200

    small fix

--- db/access.php
+++ db/access.php
@@ -6,17 +6,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $capabilities = array(
 
-    'mod/openchat:view' => array(
-        'captype' => 'write',
-        'contextlevel' => CONTEXT_MODULE,
-        'archetypes' => array(
-            'guest' => CAP_ALLOW,
-            'user' => CAP_ALLOW,
-            'frontpage' => CAP_ALLOW
-        )
-    ),
- 
-
     'mod/openchat:addinstance' => array(
         'riskbitmask' => RISK_XSS,
         'captype' => 'write',
@@ -26,6 +15,42 @@ $capabilities = array(
             'manager' => CAP_ALLOW
         ),
         'clonepermissionsfrom' => 'moodle/course:manageactivities'
-    )
+    ),
+
+    'mod/openchat:view' => array(
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_MODULE,
+        'archetypes' => array(
+            'guest' => CAP_ALLOW,
+            'frontpage' => CAP_ALLOW,
+            'student' => CAP_ALLOW,
+            'teacher' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW,
+        )
+    ),
+ 
+    'mod/openchat:read' => array(
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_MODULE,
+        'archetypes' => array(
+            'guest' => CAP_ALLOW,
+            'frontpage' => CAP_ALLOW,
+            'student' => CAP_ALLOW,
+            'teacher' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW,
+        )
+    ),
+
+    'mod/openchat:edit' => array(
+        'riskbitmask' => RISK_XSS,
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_MODULE,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW,
+        )
+    ),
     
 );
